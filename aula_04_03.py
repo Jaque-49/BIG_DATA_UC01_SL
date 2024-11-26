b62000de9e781aec1409c6d266793cf0cf8ac075
Jaque-49 <jackmonteiro28@yahoo.com.br>
2024-11-25 21:28:47 -0300

    Add files via upload

--- aula_04_03.py
+++ aula_04_03.py
@@ -1,10 +1,12 @@
-#
+#Programa que realiza a soma dentre 5 números inteiros e fornece o maior
 soma = 0
 maior = 0
 for i in range(5):
     #inicio do bloco
     num = int(input("informe o valor:"))
     if num > maior:
+        maior = num
     soma = soma + num #acumulador
     # fim do bloco
 print (f"o Resultado da soma é: {soma}") 
+print (f"o maior numero encontrado é: {maior}") 
